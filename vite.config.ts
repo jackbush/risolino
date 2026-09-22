ddc9904050610b08cba5d38f4ddd75d4d3792e25
JB <jack.bush@me.com>
2026-09-22 10:17:56 +0100

    rename riso -> risolino

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/riso/',
+  base: '/risolino/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
